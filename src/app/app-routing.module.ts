931a842f14040d775a653a0a95e93e04032c52cd
Alejandro Lopez <lopezalfaro94@gmail.com>
2020-12-01 15:23:40 -0600

    Diseño de la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SliderComponent } from './components/slider/slider.component';
+import { AboutComponent } from './components/about/about.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'slider', component: SliderComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'about'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { AboutComponent } from './components/about/about.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    AboutComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
